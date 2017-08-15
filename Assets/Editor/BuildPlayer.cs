39e074d9df7ebd68a36cda21a856fe82860abc58
Pat Wilson <zerostride@gmail.com>
2017-08-15 12:55:58 -0700

    Support Unity 5

--- Assets/Editor/BuildPlayer.cs
+++ Assets/Editor/BuildPlayer.cs
@@ -1,3 +1,8 @@
+#if UNITY_3_5 || UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7
+#else
+#  define UNITY_5
+#endif
+
 using System.IO;
 using UnityEditor;
 
@@ -16,7 +21,10 @@ class BuildPlayer
     {
         string buildPath = "iOSBuild";
         Directory.CreateDirectory(buildPath);
-
+#if UNITY_5
+        BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.iOS, BuildOptions.Development);
+#else
         BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.iPhone, BuildOptions.Development);
+#endif
     }
 }
